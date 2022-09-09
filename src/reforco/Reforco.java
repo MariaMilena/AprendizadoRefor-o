d6916d3a9483b56b5f16ca637dfb80f4382f08f2
Milena <milena20182018@gmail.com>
2022-09-09 02:52:06 -0300

    Aplicando a formula q learning

--- src/reforco/Reforco.java
+++ src/reforco/Reforco.java
@@ -6,6 +6,7 @@
 package reforco;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -36,10 +37,13 @@ public class Reforco {
                            {0, 0, 0, 0},
                           };*/
         
+        double q = 0;
+        String direcao = ""; 
+        double maiorValor = 0;
         Cell [][]cellMat = {{new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0)},
                             {new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0)},
                             {new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0)},
-                            {new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0)},
+                            {new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0), new Cell(0, 0, 0, 0, 0, 0), new Cell(10, 0, 0, 0, 0, 0)},
                            };
 
         do {
@@ -55,12 +59,40 @@ public class Reforco {
         
         while(true){
             if(horizontal == coluna-1 && vertical == linha-1){
-                System.out.println("CHEGOUUUUUUU");
      
+                System.out.println("########");
+                for(int i=0; i<linha; i++){
+                    for(int j=0; j<coluna; j++){
+                        System.out.print(cellMat[i][j].valor);
+                        System.out.print(" ");
+                    }
+                    System.out.println();
+                }
+                System.out.println("CHEGOUUUUUUU");
+
+                /*System.out.println("########");
+                for(int i=0; i<linha; i++){
+                    for(int j=0; j<coluna; j++){
+                        System.out.print("Up:"+cellMat[i][j].valorUp+"Down: "+cellMat[i][j].valorDown+"Left: "+cellMat[i][j].valorLeft+"Right: "+cellMat[i][j].valorRight);
+                        System.out.print("/");
+                    }
+                    System.out.println();
+                }*/
+                
                 pos_y = numberAleatorio.nextInt(linha); 
                 pos_x = numberAleatorio.nextInt(coluna);
                 cellMat[pos_y][pos_x].valor = 2;
-                break;
+                cellMat[vertical][horizontal].valor = 0;
+
+                horizontal = pos_x;
+                vertical = pos_y; 
+
+                try {
+                    TimeUnit.SECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
             System.out.println("########");
             for(int i=0; i<linha; i++){
@@ -73,34 +105,74 @@ public class Reforco {
             cellMat[vertical][horizontal].valor = 0;
             auxHorizontal = horizontal;
             auxVertical = vertical;
-            do {
-                if(cellMat[vertical][horizontal].valor == 1){
-                    horizontal = auxHorizontal;
-                    vertical = auxVertical;
-                }
-                if(numberAleatorio.nextInt(2) == 0) {
-                    do{
-                        if(numberAleatorio.nextInt(2) == 0){
-                            if(horizontal != 0)
-                                horizontal -= 1;
-                        } else {
-                            if(horizontal != coluna-1)
-                                horizontal+=1;
-                        }
-                    } while(auxHorizontal == horizontal);
+
+            maiorValor = Math.max(cellMat[vertical][horizontal].valorRight, Math.max(cellMat[vertical][horizontal].valorLeft, Math.max(cellMat[vertical][horizontal].valorDown, cellMat[vertical][horizontal].valorUp)));
+            
+            if(maiorValor == 0) {
+                do {
+                    if(cellMat[vertical][horizontal].valor == 1){
+                        horizontal = auxHorizontal;
+                        vertical = auxVertical;
+                    }
+                    if(numberAleatorio.nextInt(2) == 0) {
+                        do{
+                            if(numberAleatorio.nextInt(2) == 0){
+                                if(horizontal != 0){
+                                    horizontal -= 1;
+                                    direcao = "l";
+                                }
+                            } else {
+                                if(horizontal != coluna-1){
+                                    horizontal+=1;
+                                    direcao = "r";
+                                }
+                            }
+                        } while(auxHorizontal == horizontal);
+                    } else {
+                        do{
+                            if(numberAleatorio.nextInt(2) == 0){
+                                if(vertical != 0){
+                                    vertical -= 1;
+                                    direcao = "d";
+                                }
+                            } else {
+                                if(vertical != linha-1){
+                                    vertical+=1;
+                                    direcao = "u";
+                                }
+                            }
+                        } while(auxVertical == vertical);
+                    }
+                } while (cellMat[vertical][horizontal].valor == 1);
+                cellMat[vertical][horizontal].valor = 2; 
+            } else {
+                if(cellMat[vertical][horizontal].valorRight >= cellMat[vertical][horizontal].valorLeft 
+                        && cellMat[vertical][horizontal].valorRight >= cellMat[vertical][horizontal].valorUp 
+                        && cellMat[vertical][horizontal].valorRight >= cellMat[vertical][horizontal].valorDown){
+                    horizontal+=1;
+                } else if(cellMat[vertical][horizontal].valorLeft >= cellMat[vertical][horizontal].valorRight 
+                        && cellMat[vertical][horizontal].valorLeft >= cellMat[vertical][horizontal].valorUp 
+                        && cellMat[vertical][horizontal].valorLeft >= cellMat[vertical][horizontal].valorDown){
+                    horizontal-=1;
+                } else if(cellMat[vertical][horizontal].valorUp >= cellMat[vertical][horizontal].valorRight 
+                        && cellMat[vertical][horizontal].valorUp >= cellMat[vertical][horizontal].valorLeft 
+                        && cellMat[vertical][horizontal].valorUp >= cellMat[vertical][horizontal].valorDown){
+                    vertical+=1;
                 } else {
-                    do{
-                        if(numberAleatorio.nextInt(2) == 0){
-                            if(vertical != 0)
-                                vertical -= 1;
-                        } else {
-                            if(vertical != linha-1)
-                                vertical+=1;
-                        }
-                    } while(auxVertical == vertical);
+                    vertical-=1;
                 }
-            } while (cellMat[vertical][horizontal].valor == 1);
-            cellMat[vertical][horizontal].valor = 2; 
+            }
+
+            q = cellMat[vertical][horizontal].r + (0.9 * Math.max(cellMat[vertical][horizontal].valorRight, Math.max(cellMat[vertical][horizontal].valorLeft, Math.max(cellMat[vertical][horizontal].valorDown, cellMat[vertical][horizontal].valorUp))));
+            
+            if(direcao == "e")
+                cellMat[auxVertical][auxHorizontal].valorLeft = q;
+            else if(direcao == "r")
+                cellMat[auxVertical][auxHorizontal].valorRight = q;
+            else if(direcao == "d")
+                cellMat[auxVertical][auxHorizontal].valorDown = q;
+            else
+                cellMat[auxVertical][auxHorizontal].valorUp = q;
         }
     }
     
